2af3a5c753ecf581a3a118ac7b4f58ec9092d9b4
k-miloo <jjkasjanova@gmail.com>
2025-02-28 13:51:33 +0200

    Registration form test

---
+++ tests/registration.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import * as selectors from './selectors';
+
+const name = 'Name';
+const email = 'example@example.com';
+const password = 'Tokyo098123';
+
+test('Verify registration form with valid credentials', async ({ page }) => {
+
+    // Navigate to Home Page
+  await page.goto('https://auth-home-task.vercel.app/');
+
+  // Click on Go to Registration button
+  await page.locator(selectors.goToBtn).click();
+
+  // Fill in all inputs with valid data
+  await page.locator(selectors.nameInput).fill(name);
+  await page.locator(selectors.emailInput).fill(email);
+  await page.locator(selectors.passwordInput).fill(password);
+  await page.locator(selectors.confirmPasswordInput).fill(password);
+
+  // Click on Register button 
+  await page.locator(selectors.registerBtn).click();
+
+  // Verify the success message
+  await expect(page.locator(selectors.successMessage)).toHaveText('You have registered successfully!');
+
+});
---
+++ tests/selectors.js
@@ -0,0 +1,7 @@
+export const goToBtn = '//button[contains(text(), "Go to Registration")]'
+export const nameInput = '//input[contains(@name, "username")]';
+export const emailInput = '//input[contains(@name, "email")]';
+export const passwordInput = '//input[contains(@name, "password")]';
+export const confirmPasswordInput = '//input[contains(@name, "confirmPassword")]';
+export const registerBtn = '//button[contains(text(), "Register")]';
+export const successMessage = '//p[contains(@class, "text-green-500 mb-4")]';
