d3cd67857f7d6eb8ca51affc155c7f3ef3a92017
k-miloo <jjkasjanova@gmail.com>
2025-03-01 11:36:52 +0200

    failed registration test

--- tests/selectors.js
+++ tests/selectors.js
@@ -5,3 +5,4 @@ export const passwordInput = '//input[contains(@name, "password")]';
 export const confirmPasswordInput = '//input[contains(@name, "confirmPassword")]';
 export const registerBtn = '//button[contains(text(), "Register")]';
 export const successMessage = '//p[contains(@class, "text-green-500 mb-4")]';
+export const errorMessage = '//p[contains(@class, "text-red-500 text-sm mt-1")]';
